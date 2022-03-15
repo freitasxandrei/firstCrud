6725e69de81af474cea00498283187dd6e65f13d
Andrei de Freitas <freitasxandrei@gmail.com>
2022-03-14 21:48:40 -0300

    firstCrud

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <title> FREITASXANDREI </title>
+  </head>
+  <body class="bg-dark text-light">
+  <div class="container">
+  <header class="jumbotron bg-danger">
+    <h1 class="display-4"> Olá meu caro! </h1>
+    <p class="lead"> Busque a humilhação. </p>
+  </header>
---
+++ includes/listagem.php
@@ -0,0 +1,34 @@
+<section>
+
+    <table class="table bg-light mt-03">
+        <thead>
+            <tr>
+                <th> ID </th>
+                <th> Título </th>
+                <th> Descrição </th>
+                <th> Data </th>
+                <th> Status </th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td> 1 </td>
+                <td> Teste Título 1 </td>
+                <td> Descrição Teste 1 </td>
+                <td> 11/11/2011 11:11:11 </td>
+                <td> Ativo </td>
+            </tr>
+        </tbody>
+
+        <tbody>
+            <tr>
+                <td> 2 </td>
+                <td> Teste Título 2 </td>
+                <td> Descrição Teste 2 </td>
+                <td> 11/11/2011 11:11:11 </td>
+                <td> Inativo </td>
+            </tr>
+        </tbody>
+
+</section>
