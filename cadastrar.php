e788b2706dadf51aba035627e8af62c4e061bb3b
Andrei de Freitas <freitasxandrei@gmail.com>
2022-03-15 21:50:27 -0300

    fourthCommit

--- cadastrar.php
+++ cadastrar.php
@@ -1,8 +1,7 @@
 <?php 
     require __DIR__.'/vendor/autoload.php';
-
+    
     require __DIR__.'/includes/header.php';
     require __DIR__.'/includes/formulario.php';
     require __DIR__.'/includes/footer.php';
 ?>
-
